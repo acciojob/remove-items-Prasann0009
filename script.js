f14518c4ef222df5fce31df32d4ae3a29a906fd3
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-10-25 19:41:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function removeO()
+	{
+		const select = document.querySelector("select");
+		const selectedValue = select.value;
+
+		let options = select.children;
+
+		for(let i=0;i<options.length;i++)
+			{
+					if(options[i].value === selectedValue)
+				{
+					options[i].remove();
+					break;
+				}
+			}
+	}
